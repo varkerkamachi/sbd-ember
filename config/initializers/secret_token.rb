409034c37924f04a02c831e215f1a337e04624a0
Andrew Cranston <andrewtcranston@gmail.com>
2014-05-10 23:44:24 -0700

    initial rails install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sbd::Application.config.secret_key_base = 'f94b84b601f247cba4dea6537da76a32b21015558887f23688d3d7bb9a4901077d2f2afb688777b6c34e9f2a56def9df065ddae7cc63313bd1589b5d895f3e85'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sbd::Application.config.session_store :cookie_store, key: '_sbd_session'
