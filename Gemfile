d8271469c2bc8632a564db0c596172cd775811bd
Andrew Cranston <andrewtcranston@gmail.com>
2014-05-11 17:50:55 -0700

    adding ember data lib and gem

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ source 'https://rubygems.org'
 gem 'rails', '4.0.3'
 
 gem 'ember-rails'
+gem 'ember-data-source'
 
 # Use sqlite3 as the database for Active Record
 # gem 'sqlite3'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,13 +13,10 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require ./vendor/ember.min
-//= require ./vendor/handlebars
-//= require_tree ./lib
+//= require_tree ./vendor
 //= require app/app
 //= require_tree ./app/models
 //= require_tree ./app/controllers
 //= require_tree ./app/views
-//= require_tree ./app/helpers
 //= require_tree ./app/templates
 //= require_tree .
